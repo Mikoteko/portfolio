ce354b37ab5193f3cfad02f893ec86cd47c56d25
epacheco <emiliepacheco@yahoo.com>
2023-11-17 18:40:18 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,23 @@
+# Project: Fantastic 3D Portfolio Website (using ThreeJS)
+
+ORIGINAL BY yennhi26 on Github 🌼 https://github.com/yennhi26/3d_portfolio
+
+## Introduction
+
+The most impressive websites in the world use 3D graphics and animations to bring my content to life. 
+
+## Tech stack
+- ThreeJS 
+- React Three Fiber
+
+- TailwindCSS 
+- Framer Motion
+
+## Features
+- Load, create and customize stunning 3D models and geometries with various lights, as well as understand the 3D world with a camera and positioning of an object in space.
+
+- Make your code reusable and scalable using Higher Order Components (HOCs) and other industry-standard best practices
+
+- Implement sending emails through a form on the website
+
+- Ensure responsiveness across all devices and improve your site's performance using Suspense and Preload.
---
+++ src/constants/index.js
@@ -0,0 +1,233 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    fsoft,
+    hahalolo,
+    appcore,
+    portfolio,
+    photo_app,
+    crypto
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "À propos",
+    },
+    {
+      id: "work",
+      title: "Expériences professionnelles",
+    }
+  ];
+  
+  const services = [
+    {
+      title: "Frontend Developer",
+      icon: web,
+    },
+    {
+      title: "Web Developer",
+      icon: backend,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Social Media Manager",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Frontend Developer",
+      company_name: "FPT Software",
+      icon: fsoft,
+      iconBg: "#383E56",
+      date: "July 2021 - June 2023",
+      points: [
+        "Communicated with teammates to clarify requirements.",
+        "Developed, implemented and maintained production code for client websites.",
+        "Created unit tests and reported them.",
+        "Wrote detailed designs for business flows.",
+      ],
+    },
+    {
+      title: "Frontend Developer",
+      company_name: "Hahalolo LLC",
+      icon: hahalolo,
+      iconBg: "#E6DEDD",
+      date: "Feb 2020 - Jul 2021",
+      points: [
+        "Mentored new member of the team.",
+        "Collaborated with designers on projects to create brands, design systems, and websites for organizations.",
+        "Implemented responsive design and ensuring cross-browser compatibility.",
+        "Developed, maintained, and upgraded code for systems.",
+      ],
+    },
+    {
+      title: "Web Developer",
+      company_name: "AppCore Technologies Ltd.",
+      icon: appcore,
+      iconBg: "#383E56",
+      date: "Jul 2019 - Dec 2019",
+      points: [
+        "Built a system that meets client's requirements.",
+        "Translated design wireframes into functioning UI components.",
+        "Integrated API and developed code.",
+      ],
+    },
+  ];
+  
+  const testimonials = [];
+  //   {
+  //     testimonial:
+  //       "I've never met a web developer who truly cares about their clients' success like Lyra does.",
+  //     name: "Sara Le",
+  //     designation: "PM",
+  //     company: "FPT Software",
+  //     image: "https://randomuser.me/api/portraits/women/4.jpg",
+  //   },
+  //   {
+  //     testimonial:
+  //       "I thought it was impossible to make a website as beautiful as our product, but Lyra proved me wrong.",
+  //     name: "Chris Brown",
+  //     designation: "COO",
+  //     company: "FPT Software",
+  //     image: "https://randomuser.me/api/portraits/men/5.jpg",
+  //   },
+  //   {
+  //     testimonial:
+  //       "After Lyra optimized our website, our traffic increased by 50%. We can't thank them enough!",
+  //     name: "Lisa Wang",
+  //     designation: "CTO",
+  //     company: "FPT Software",
+  //     image: "https://randomuser.me/api/portraits/women/6.jpg",
+  //   },
+  // ];
+  
+  const projects = [
+    {
+      name: "3D Developer Portfolio",
+      description:
+        "The most impressive websites in the world use 3D graphics and animations to bring my content to life.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "threejs",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: portfolio,
+      source_code_link: "https://github.com/yennhi26/3d_portfolio",
+    },
+    {
+      name: "Crytocurrency App",
+      description:
+        "Web application that uses Redux Toolkit to bring crypto to life.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "redux toolkit",
+          color: "green-text-gradient",
+        },
+      ],
+      image: crypto,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Awesome Photo App",
+      description:
+        "An app features randomizing photos and storing photos using Redux.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "redux toolkit",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: photo_app,
+      source_code_link: "https://github.com/yennhi26/photo-app",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
